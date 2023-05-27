b729dc8c6794c43e66b03204bff96b6f26aee877
Joao Pinto <joaopinto@joaos-mbp.lan>
2023-05-27 14:44:28 +0100

    Initialize project using Create React App

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-multiple-empty-lines': 'error',
+    'react/prop-types': 'off',
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: {
+          multiline: true,
+        },
+        ImportDeclaration: 'never',
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+    'react/jsx-one-expression-per-line': 'off',
+    'no-unused-vars': 'warn',
+    'react/jsx-no-bind': 'warn',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
+
+
