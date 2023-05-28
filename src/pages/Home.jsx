ddf0ccb0343b9795e46290193e74dc83063df7d7
Joao Pinto <joaopinto@joaos-mbp.lan>
2023-05-28 16:11:55 +0100

    feat: adds navigation with nav-menu component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div
+      style={{
+        backgroundColor: 'yellow',
+        width: '100%',
+        minHeight: 'calc(100vh - 80px)',
+      }}
+    />
+  );
+}
+
+export default Home;
