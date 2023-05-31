563815a1a19803ac9cefa10e8fa8216f1634a0f3
Joao Pinto <pinftf@gmail.com>
2023-05-31 06:42:08 +0100

    Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,23 +1,77 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
 import './Carousel.css';
 import Image1 from '../images/star-wars-stormtroopers-Edouard-Groult.jpg';
 import Image2 from '../images/Star_Wars_Art_Concept_Illustration_M01.jpg';
 
 function Carousela() {
   return (
-    <Carousel fade>
-      <Carousel.Item>
-        <img className="d-block w-100" src={Image1} alt="First Slide" />
-        <Carousel.Caption>
-          <h3>You shall not pass</h3>
-          <p>Brief description</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={Image2} alt="Second Slide" />
-      </Carousel.Item>
-    </Carousel>
+    <section className="carousel" aria-label="Gallery">
+      <ol className="carousel__viewport">
+        <li id="carousel__slide1" className="carousel__slide">
+          <img src={Image1} alt="Image1" />
+          <div className="carousel__snapper">
+            <a href="#carousel__slide4" className="carousel__prev">
+              Go to last slide
+            </a>
+            <a href="#carousel__slide2" className="carousel__next">
+              Go to next slide
+            </a>
+          </div>
+        </li>
+        <li id="carousel__slide2" className="carousel__slide">
+          <img src={Image2} alt="Image2" />
+          <div className="carousel__snapper" />
+          <a href="#carousel__slide1" className="carousel__prev">
+            Go to previous slide
+          </a>
+          <a href="#carousel__slide3" className="carousel__next">
+            Go to next slide
+          </a>
+        </li>
+        <li id="carousel__slide3" className="carousel__slide">
+          <div className="carousel__snapper" />
+          <a href="#carousel__slide2" className="carousel__prev">
+            Go to previous slide
+          </a>
+          <a href="#carousel__slide4" className="carousel__next">
+            Go to next slide
+          </a>
+        </li>
+        <li id="carousel__slide4" className="carousel__slide">
+          <div className="carousel__snapper" />
+          <a href="#carousel__slide3" className="carousel__prev">
+            Go to previous slide
+          </a>
+          <a href="#carousel__slide1" className="carousel__next">
+            Go to first slide
+          </a>
+        </li>
+      </ol>
+      <aside className="carousel__navigation">
+        <ol className="carousel__navigation-list">
+          <li className="carousel__navigation-item">
+            <a href="#carousel__slide1" className="carousel__navigation-button">
+              Go to slide 1
+            </a>
+          </li>
+          <li className="carousel__navigation-item">
+            <a href="#carousel__slide2" className="carousel__navigation-button">
+              Go to slide 2
+            </a>
+          </li>
+          <li className="carousel__navigation-item">
+            <a href="#carousel__slide3" className="carousel__navigation-button">
+              Go to slide 3
+            </a>
+          </li>
+          <li className="carousel__navigation-item">
+            <a href="#carousel__slide4" className="carousel__navigation-button">
+              Go to slide 4
+            </a>
+          </li>
+        </ol>
+      </aside>
+    </section>
   );
 }
 
