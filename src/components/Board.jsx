7c3f6578d2134f849a0ef68fb1c40296b3979873
Joao Pinto <pinftf@gmail.com>
2023-06-05 20:43:42 +0100

    feat: soundboard done (misses css)

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SoundButton from './SoundButton';
+import './Board.css';
 
 function Board() {
   const sounds = [
@@ -7,21 +8,32 @@ function Board() {
       name: 'Lightsaber',
 
       src: '../sounds/lightsaber.mp3',
+
+      image: `${process.env.PUBLIC_URL}/images/lightsaber.jpg`,
     },
     {
       name: 'Chewbacca',
 
       src: '../sounds/chewbacca.mp3',
+
+      image: `${process.env.PUBLIC_URL}/images/chewbacca.jpg`,
     },
   ];
   return (
-    <div className="soundboard">
-      <h2>Star Wars Soundboard</h2>
-      <div className="sound-buttons">
-        {sounds.map((sound) => (
-          // eslint-disable-next-line react/jsx-no-undef
-          <SoundButton key={sound.name} soundSrc={sound.src} />
-        ))}
+    <div className="container">
+      <div className="soundboard">
+        <h2>Star Wars Soundboard</h2>
+        <div className="sound-buttons">
+          {sounds.map((sound) => (
+            // eslint-disable-next-line react/jsx-no-undef
+            <SoundButton
+              key={sound.name}
+              soundSrc={sound.src}
+              title={sound.name}
+              image={sound.image}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/SoundButton.jsx
+++ src/components/SoundButton.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import './SoundButton.css';
 
-function SoundButton({ soundSrc }) {
+function SoundButton({ soundSrc, title, image }) {
   const playSound = () => {
     const audio = new Audio(soundSrc);
     audio.play();
   };
+
+  const buttonStyle = {
+    backgroundImage: `url(${image})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+
   return (
-    <button type="button" className="sound-button" onClick={playSound}>
-      Play Sound
+    <button
+      type="button"
+      className="sound-button"
+      onClick={playSound}
+      style={buttonStyle}
+    >
+      {title}
     </button>
   );
 }
